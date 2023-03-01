2a3521780fa17ff34b7b9159085b6051a253fe07
SiriusKoan <k.peihsun@gmail.com>
2023-03-01 20:25:39 +0800

    dump three types of hw to dict

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const submit_btn = document.getElementById("submit-session");
 const port = chrome.runtime.connect({ name: "connection" });
 
 var in_progress = [];
+var submitted = [];
+var overdue = [];
 
 port.onMessage.addListener(function (response) {
   if (response["type"] == "main") {
@@ -18,10 +20,11 @@ port.onMessage.addListener(function (response) {
     }
   }
   else if (response["type"] == "course") {
-   // homework in one class
+    // homework in one class
     let html = document.createElement("div");
     html.setAttribute('style', 'display: none;');
     html.innerHTML = response["data"];
+    // in_progress
     let in_progress_names = html.querySelectorAll("#news-view-basic-in-progress tbody tr .instancename")
     let in_progress_start_dates = html.querySelectorAll("#news-view-basic-in-progress tbody tr td:nth-child(2)")
     let in_progress_due_dates = html.querySelectorAll("#news-view-basic-in-progress tbody tr td:nth-child(3)")
@@ -36,7 +39,35 @@ port.onMessage.addListener(function (response) {
         "link": in_progress_links[i].innerText,
       });
     }
-    console.log(in_progress);
+    // submitted
+    let submitted_names = html.querySelectorAll("#news-view-nofile2-tobegraded-in-progress tbody tr .instancename")
+    let submitted_start_dates = html.querySelectorAll("#news-view-nofile2-tobegraded-in-progress tbody tr td:nth-child(2)")
+    let submitted_due_dates = html.querySelectorAll("#news-view-nofile2-tobegraded-in-progress tbody tr td:nth-child(3)")
+    let submitted_students_counts = html.querySelectorAll("#news-view-nofile2-tobegraded-in-progress tbody tr td:nth-child(4)")
+    let submitted_links = html.querySelectorAll("#news-view-nofile2-tobegraded-in-progress tbody tr .aalink")
+    for (let i = 0; i < submitted_names.length; i++) {
+      submitted.push({
+        "name": submitted_names[i].innerText,
+        "start_date": submitted_start_dates[i].innerText,
+        "due_date": submitted_due_dates[i].innerText,
+        "students_count": submitted_students_counts[i].innerText,
+        "link": submitted_links[i].innerText,
+      });
+    }
+    // overdue
+    let overdue_names = html.querySelectorAll("#news-view-nofile2-notsubmitted-in-progress tbody tr .instancename")
+    let overdue_start_dates = html.querySelectorAll("#news-view-nofile2-notsubmitted-in-progress tbody tr td:nth-child(2)")
+    let overdue_due_dates = html.querySelectorAll("#news-view-nofile2-notsubmitted-in-progress tbody tr td:nth-child(3)")
+    let overdue_students_counts = html.querySelectorAll("#news-view-nofile2-notsubmitted-in-progress tbody tr td:nth-child(4)")
+    let overdue_links = html.querySelectorAll("#news-view-nofile2-notsubmitted-in-progress tbody tr .aalink")
+    for (let i = 0; i < overdue_names.length; i++) {
+      overdue.push({
+        "name": overdue_start_dates[i].innerText,
+        "due_date": overdue_due_dates[i].innerText,
+        "students_count": overdue_students_counts[i].innerText,
+        "link": overdue_links[i].innerText,
+      });
+    }
   }
 });
 
